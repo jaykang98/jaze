10154120f75834c9ee4e5f5689032e628f513c52
j_aykang98 <juleskang2@gmail.com>
2024-02-24 14:26:59 -0600

    [

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+// Example definitions within types.ts
+
+export interface FormData {
+    artist: string;
+    album: string;
+    track: string;
+    startTimestamp: string;
+    endTimestamp: string;
+}
+
+export interface Options {
+    artists: string[];
+    albums: string[];
+    tracks: string[];
+}
+
---
+++ src/ui/inputSelection/InputSelection.tsx
@@ -0,0 +1,78 @@
+// InputSelection.tsx
+import React from 'react';
+import Input from '../../ui/input/Input';
+import OptionList from '../../ui/optionList/OptionList';
+import TimeSelectionRow from '../../ui/timeSelectionRow/TimeSelectionRow';
+import { FormData, Options } from '../../types';
+
+type SelectionType = "artist" | "album" | "track";
+
+interface InputSelectionProps {
+    selectionType: SelectionType;
+    formData: FormData;
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    handleTypeChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+    options: Options;
+    handleOptionSelect: (type: keyof FormData, option: { name: string }) => void;
+    userID?: string;
+}
+
+const InputSelection: React.FC<InputSelectionProps> = ({
+    selectionType,
+    formData,
+    handleChange,
+    handleTypeChange,
+    options,
+    handleOptionSelect,
+}) => (
+    <>
+        <tr>
+            <td>
+                <select value={selectionType} onChange={handleTypeChange}>
+                    <option value="track">Track</option>
+                    <option value="artist">Artist</option>
+                    <option value="album">Album</option>
+                </select>
+            </td>
+            <td>
+                <Input
+                    id={selectionType}
+                    type="text"
+                    name={selectionType}
+                    value={formData[selectionType]}
+                    onChange={handleChange}
+                    placeholder={`Enter ${selectionType} name`}
+                />
+            </td>
+            <td>
+                <OptionList
+                    options={options[`${selectionType}s`].map((name) => ({ name }))}
+                    onSelect={(option) => handleOptionSelect(selectionType, option)}
+                />
+            </td>
+        </tr>
+        <tr>
+            <td>Start Time</td>
+            <td colSpan={2}>
+                <TimeSelectionRow
+                    label="Start Time"
+                    timestamp={formData.startTimestamp}
+                    onYearSelect={(year) => console.log(year)} // Adjust based on actual functionality
+                />
+            </td>
+        </tr>
+        <tr>
+            <td>End Time</td>
+            <td colSpan={2}>
+                <TimeSelectionRow
+                    label="End Time"
+                    timestamp={formData.endTimestamp}
+                    onYearSelect={(year) => console.log(year)} // Adjust based on actual functionality
+                />
+            </td>
+        </tr>
+
+    </>
+);
+
+export default InputSelection;
