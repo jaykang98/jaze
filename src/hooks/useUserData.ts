d2b1ec5f7eebd41482c27de133c4b283fa4d186f
j_aykang98 <juleskang2@gmail.com>
2024-03-01 19:52:41 -0600

    Fixed useUserData

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,12 +1,12 @@
 // FileName: src/hooks/useUserData.ts
 import { useState, useEffect } from "react";
 import { fetchAndProcessData } from "./utils/dataHandler";
-
+import { AlbumData, ArtistData, TrackData } from "types/dataTypes"
 export const useUserData = (username: string) => {
-    const [userInfo, setUserInfo] = useState<any | null>(null);
-    const [userTopAlbums, setUserTopAlbums] = useState<any | null>(null);
-    const [userTopArtists, setUserTopArtists] = useState<any | null>(null);
-    const [userTopTracks, setUserTopTracks] = useState<any | null>(null);
+    const [UserData, setUserInfo] = useState<AlbumData | null>(null);
+    const [AlbumData, setUserTopAlbums] = useState<AlbumData | null>(null);
+    const [ArtistData, setUserTopArtists] = useState<ArtistData | null>(null);
+    const [TrackData, setUserTopTracks] = useState<TrackData | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
@@ -14,14 +14,10 @@ export const useUserData = (username: string) => {
 
         const fetchData = async (method: string, params: object) => {
             try {
-                // Debug: Print the request details if REACT_APP_IS_DEBUG is TRUE
                 if (process.env.REACT_APP_IS_DEBUG === 'TRUE') {
                     console.log(`Fetching data with method: ${method}`, params);
                 }
-
                 const data = await fetchAndProcessData(method, params);
-
-                // Debug: Print the received data if REACT_APP_IS_DEBUG is TRUE
                 if (process.env.REACT_APP_IS_DEBUG === 'TRUE') {
                     console.log(`Received data for method: ${method}`, data);
                 }
@@ -30,38 +26,38 @@ export const useUserData = (username: string) => {
             } catch (error) {
                 console.error(`Failed to fetch data for method: ${method}`, error);
                 setError(`Failed to fetch data for method: ${method}`);
-                return null; // Ensure we return null on error to maintain state consistency
+                return null; 
             }
         };
 
         const fetchUserInfo = async () => {
-            const userInfo = await fetchData("user.getinfo", { user: username });
-            setUserInfo(userInfo);
+            const UserData = await fetchData("user.getinfo", { user: username });
+            setUserInfo(UserData);
         };
 
         const fetchUserTopAlbums = async () => {
-            const userTopAlbums = await fetchData("user.getTopAlbums", {
+            const AlbumData = await fetchData("user.getTopAlbums", {
                 user: username,
                 limit: 5,
                 period: "12month",
-            });
-            setUserTopAlbums(userTopAlbums);
+            }) as AlbumData;
+            setUserTopAlbums(AlbumData);
         };
 
         const fetchUserTopArtists = async () => {
-            const userTopArtists = await fetchData("user.getTopArtists", {
+            const ArtistData = await fetchData("user.getTopArtists", {
                 user: username,
                 limit: 5,
-            });
-            setUserTopArtists(userTopArtists);
+            }) as ArtistData;
+            setUserTopArtists(ArtistData);
         };
 
         const fetchUserTopTracks = async () => {
-            const userTopTracks = await fetchData("user.getTopTracks", {
+            const TrackData = await fetchData("user.getTopTracks", {
                 user: username,
                 limit: 5,
-            });
-            setUserTopTracks(userTopTracks);
+            }) as TrackData;
+            setUserTopTracks(TrackData);
         };
 
         fetchUserInfo();
@@ -71,10 +67,10 @@ export const useUserData = (username: string) => {
     }, [username]);
 
     return {
-        userInfo,
-        userTopAlbums,
-        userTopArtists,
-        userTopTracks,
+        UserData,
+        AlbumData,
+        ArtistData,
+        TrackData,
         error,
     };
 };
