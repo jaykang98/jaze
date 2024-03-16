93ab069adb1d59abc514c3d297a3d0a472839c57
j_aykang98 <juleskang2@gmail.com>
2024-03-16 07:35:29 -0500

    f

--- src/Application/Tops/Tops.tsx
+++ src/Application/Tops/Tops.tsx
@@ -5,64 +5,45 @@ import { fetchUserData } from "../../hooks/dataManagement/fetchUserData";
 import DisplayGrid from "../../components/views/displayGrid/DisplayGrid";
 import DisplayTable from "../../components/views/displayTable/DisplayTable";
 import { ActivityConstructorProps } from "../../types/structureTypes";
-import styles from "./Tops.module.css";
 import { useViewTitle } from "../../contexts/ViewTitleContexts";
-
+import AlbumCard from "../../components/jaze/albumCard/AlbumCard"; 
 
 const Tops: React.FC<ActivityConstructorProps> = ({ userID }) => {
     const { setTitle } = useViewTitle();
 
     useEffect(() => {
-        setTitle("Tops of ALL TIME"); 
+        setTitle("Tops of ALL TIME");
     }, [setTitle]);
+
     const { userData, albumData, artistData, trackData, error, loading } = fetchUserData(userID);
 
     const formatNumber = (number: number) => new Intl.NumberFormat().format(number);
 
     const getLargeImage = (images: Array<{ size: string; "#text": string }>) =>
         images.find((image) => image.size === "large")?.["#text"] || "";
+
     const userImage = userData?.user?.image?.[0]["#text"];
-    const renderLargeImage = (data: any, type: 'artist' | 'album' | 'track') => {
-        let items = [];
-        switch (type) {
-            case 'artist':
-                items = data?.topartists?.artist.slice(0, 1) || [];
-                break;
-            case 'album':
-                items = data?.topalbums?.album.slice(0, 1) || [];
-                break;
-            case 'track':
-                items = data?.toptracks?.track.slice(0, 1) || [];
-                break;
-        }
 
-        return items.map((item: any) => (
-            <div key={item.name} className={styles.imageContainer}>
-                <img src={getLargeImage(item.image)} alt={item.name} className={styles.largeImage} />
-                <figcaption className={styles.caption}>{item.name}</figcaption>
-            </div>
-        ));
-    };
+    const renderItemContent = (data: any, type: 'artist' | 'album' | 'track', isTable: boolean = false) => {
+        const dataType = type === 'artist' ? data?.topartists : type === 'album' ? data?.topalbums : data?.toptracks;
+        const items = dataType[type].slice(0, isTable ? 10 : 1);
 
-    const dataToTable = (data: any, type: 'artist' | 'album' | 'track') => {
-        let items = [];
-        switch (type) {
-            case 'artist':
-                items = data?.topartists?.artist.slice(0, 10) || [];
-                break;
-            case 'album':
-                items = data?.topalbums?.album.slice(0, 10) || [];
-                break;
-            case 'track':
-                items = data?.toptracks?.track.slice(0, 10) || [];
-                break;
-        }
-
-        return <DisplayTable data={items.map((item: any, index: number) => ([
-            `${index + 1}`,
-            item.name,
-            `${formatNumber(+item.playcount)} scrobbles`,
-        ]))} />;
+        return isTable ? (
+            <DisplayTable data={items.map((item: any, index: number) => ([
+                `${index + 1}`,
+                item.name,
+                `${formatNumber(+item.playcount)} scrobbles`,
+            ]))} />
+        ) : (
+            items.map((item: any) => (
+                <AlbumCard
+                    key={item.name}
+                    src={getLargeImage(item.image)}
+                    alt={item.name}
+                    caption={item.name}
+                />
+            ))
+        );
     };
 
     if (loading) return <div>Loading...</div>;
@@ -79,12 +60,10 @@ const Tops: React.FC<ActivityConstructorProps> = ({ userID }) => {
             [<FontAwesomeIcon key="country-icon" icon={faGlobeAmericas} />, "Country", user.country],
             [<FontAwesomeIcon key="user-since-icon" icon={faCalendarAlt} />, "User Since", registrationDate.toLocaleDateString()],
             [<FontAwesomeIcon key="years-active-icon" icon={faCalendarAlt} />, "Years Active", `${yearsSinceRegistration} years`],
-            [<FontAwesomeIcon key="playcount-icon" icon={faMusic} />, "Playcount", Number(user.playcount).toLocaleString()],
+            [<FontAwesomeIcon key="playcount-icon" icon={faMusic} />, "Playcount", formatNumber(user.playcount)],
         ];
 
-        return (
-            <DisplayTable data={dataForDisplay} />
-        );
+        return <DisplayTable data={dataForDisplay} />;
     };
 
     return (
@@ -95,43 +74,43 @@ const Tops: React.FC<ActivityConstructorProps> = ({ userID }) => {
                 viewFrames={[
                     {
                         content: <>
-                            {renderLargeImage(artistData, "artist")}
+                            {renderItemContent(artistData, "artist")}
                             <h3>Your Top Artists of all time</h3>
-                            {dataToTable(artistData, "artist")}
+                            {renderItemContent(artistData, "artist", true)}
                         </>,
                         viewWidth: 100,
                     },
                     {
                         content: <>
-                            {renderLargeImage(albumData, "album")}
+                            {renderItemContent(albumData, "album")}
                             <h3>Your Top Albums of all time</h3>
-                            {dataToTable(albumData, "album")}
+                            {renderItemContent(albumData, "album", true)}
                         </>,
                         viewWidth: 100,
                     },
-
                 ]}
             />
             <DisplayGrid
                 viewFrames={[
                     {
                         content: <>
-                            {renderLargeImage(trackData, "track")}
+                            {renderItemContent(trackData, "track")}
                             <h3>Your Top Tracks of all time</h3>
-                            {dataToTable(trackData, "track")}
+                            {renderItemContent(trackData, "track", true)}
                         </>,
                         viewWidth: 100,
                     },
                     {
-                        content:
-                            <>
-                                <div key='userImage' className={styles.imageContainer}>
-                                    <img src={userImage} alt="User" className={styles.userImage} />
-                                    <figcaption className={styles.caption}>{userData.user.realname}</figcaption>
-                                </div>
-                                <h3>Who is {userData.user.name}???</h3>
-                                {renderUserInfo()}
-                            </>,                                
+                        content: <>
+                            <AlbumCard
+                                key='userImage'
+                                src={userImage}
+                                alt="User"
+                                caption={userData.user.realname}
+                            />
+                            <h3>Who is {userData.user.name}???</h3>
+                            {renderUserInfo()}
+                        </>,
                         viewWidth: 100,
                     },
                 ]}
---
+++ src/components/jaze/albumCard/AlbumCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './AlbumCard.module.css'; 
+
+interface AlbumCardProps {
+    src: string;
+    alt: string;
+    caption: string;
+}
+
+const AlbumCard: React.FC<AlbumCardProps> = ({ src, alt, caption }) => {
+    return (
+        <div className={styles.albumCard}>
+            <img src={src} alt={alt} className={styles.albumImage} />
+            <figcaption className={styles.caption}>{caption}</figcaption>
+        </div>
+    );
+};
+
+export default AlbumCard;
